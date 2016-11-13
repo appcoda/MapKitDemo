d393004260caa87112a08e1b9301d0498152c107
Simon Ng <simonng@appcoda.com>
2016-11-13 21:21:29 +0800

    Initial version

---
+++ MapKit Starter/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  MapKit Starter
+//
+//  Created by Pranjal Satija on 10/25/16.
+//  Copyright © 2016 Pranjal Satija. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+    @IBOutlet var mapView: MKMapView?
+    
+    let locationManager = CLLocationManager()
+    
+    let places = Place.getPlaces()
+    
+    override func viewDidLoad() {
+        requestLocationAccess()
+        addAnnotations()
+        
+    }
+    
+    func requestLocationAccess() {
+        let status = CLLocationManager.authorizationStatus()
+        
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            return
+            
+        case .denied, .restricted:
+            print("location access denied")
+            
+        default:
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func addAnnotations() {
+        mapView?.delegate = self
+        mapView?.addAnnotations(places)
+
+        let overlays = places.map { MKCircle(center: $0.coordinate, radius: 100) }
+        mapView?.addOverlays(overlays)
+    }
+    
+
+}
+
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+            
+        else {
+            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "annotationView") ?? MKAnnotationView()
+            annotationView.image = UIImage(named: "place icon")
+            return annotationView
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKCircleRenderer(overlay: overlay)
+        renderer.fillColor = UIColor.black.withAlphaComponent(0.5)
+        renderer.strokeColor = UIColor.blue
+        renderer.lineWidth = 2
+        return renderer
+    }
+}
